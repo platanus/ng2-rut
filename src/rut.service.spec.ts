e38bed1b72c2e5d21d5038eb504b4e03588f428d
Leandro Segovia <ldlsegovia@gmail.com>
2016-07-29 17:26:22 -0300

    tests(RutService): add specs

--- src/rut.service.spec.ts
+++ src/rut.service.spec.ts
@@ -1,4 +1,5 @@
 import { RutService } from './rut.service';
+import { it, expect, describe, addProviders, inject, beforeEach} from '@angular/core/testing';
 
 describe('RutService', () => {
   beforeEach(() => {
@@ -7,7 +8,25 @@ describe('RutService', () => {
     ]);
   });
 
-  it('cleans rut', inject([RutService], (srv: RutService) => {
-    expect(srv.cleanRut('A1234B')).toEqual('1234');
-  }));
+  describe('cleanRut', () => {
+    it('cleans rut', inject([RutService], (srv: RutService) => {
+      expect(srv.cleanRut('A12&&*~34B')).toEqual('1234');
+    }));
+  });
+
+  describe('validateRut', () => {
+    it('returns true with valid rut', inject([RutService], (srv: RutService) => {
+      expect(srv.validateRut('30972198')).toBe(true);
+    }));
+
+    it('returns false with valid rut', inject([RutService], (srv: RutService) => {
+      expect(srv.validateRut('30972199')).toBe(false);
+    }));
+  });
+
+  describe('formatRut', () => {
+    it('formats rut', inject([RutService], (srv: RutService) => {
+      expect(srv.formatRut('A30&&*~..97-2198B')).toEqual('3.097.219-8');
+    }));
+  });
 });
